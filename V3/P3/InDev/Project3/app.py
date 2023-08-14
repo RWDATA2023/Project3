39a51b3a5a34bd95228233e4297885be9d5d2d16
RWDATA2023 <rw2016job@gmail.com>
2023-08-14 19:05:33 -0400

    fingers crossed, final upload

--- V3/P3/InDev/Project3/app.py
+++ V3/P3/InDev/Project3/app.py
@@ -1,30 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask_cors import CORS
 import requests
 import urllib.parse
+import sqlite3
 
 app = Flask(__name__, static_folder='static')
-app.config['SECRET_KEY'] = '1234'  # Replace with a secure secret key
+app.config['SECRET_KEY'] = '1234'
 
-# Function to fetch weather data
-async def getWeatherData(lat, lon):
-    apiKey = 'c04ea89d7fcd4a26b5b05602230108'  # WeatherAPI API key
+CORS(app)  
+
+def getWeatherData(lat, lon):
+    apiKey = 'c04ea89d7fcd4a26b5b05602230108'
     apiUrl = f'https://api.weatherapi.com/v1/forecast.json?key={apiKey}&q={lat},{lon}&days=3&units=imperial'
-
     try:
-        response = await requests.get(apiUrl)
+        response = requests.get(apiUrl)
         data = response.json()
         return data
     except requests.RequestException as e:
         print('Error fetching weather data:', e)
         return None
 
-# Function to convert location name to coordinates using LocationIQ API
-async def getLocationCoordinates(location):
-    apiKey = 'pk.1628022f589a6bec5a2e4b2e449d83b1'  # LocationIQ API key
+def getLocationCoordinates(location):
+    apiKey = 'pk.1628022f589a6bec5a2e4b2e449d83b1'
     apiUrl = f'https://us1.locationiq.com/v1/search.php?key={apiKey}&q={urllib.parse.quote(location)}&format=json'
-
     try:
-        response = await requests.get(apiUrl)
+        response = requests.get(apiUrl)
         data = response.json()
         if len(data) > 0:
             latitude = data[0]['lat']
@@ -36,32 +36,51 @@ async def getLocationCoordinates(location):
         print('Error fetching location data:', e)
         return None, None
 
-# Home route - Serve the index.html template
+def translate_weather_conditions(weather_data, language):
+    conn = sqlite3.connect('data/weather_conditions.db')
+    cursor = conn.cursor()
+
+    # Function to translate a specific condition
+    def translate_condition(condition_text):
+        query = f"SELECT {language} FROM weather_conditions WHERE en = ?"
+        translation = cursor.execute(query, (condition_text,)).fetchone()
+        return translation[0] if translation else condition_text
+
+    # Translating current condition
+    condition_text_current = weather_data['current']['condition']['text']
+    weather_data['current']['condition']['text'] = translate_condition(condition_text_current)
+
+    # Translating forecast conditions
+    for forecast in weather_data['forecast']['forecastday']:
+        condition_text = forecast['day']['condition']['text']
+        forecast['day']['condition']['text'] = translate_condition(condition_text)
+
+    conn.close()
+    return weather_data
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Search route - Handle search requests from the form
 @app.route('/search', methods=['POST'])
-async def search():
+def search():
     location = request.form['location']
     if not location.strip():
         return 'Location name cannot be empty.'
-
-    # Use LocationIQ to convert location name to latitude and longitude
-    latitude, longitude = await getLocationCoordinates(location)
+    latitude, longitude = getLocationCoordinates(location)
     if latitude is not None and longitude is not None:
-        # Fetch weather and Geoapify data for the selected location
-        weather_data = await getWeatherData(latitude, longitude)
-        accommodations_data = await getAccommodationsData(latitude, longitude)
-        sights_data = await getSightsData(latitude, longitude)
-        food_and_drink_data = await getFoodAndDrinkData(latitude, longitude)
-
-        # Process weather_data, accommodations_data, sights_data, and food_and_drink_data as needed
-        # ...
-
+        weather_data = getWeatherData(latitude, longitude)
+        language = 'en'
+        weather_data = translate_weather_conditions(weather_data, language)
+        # ... rest of the code ...
     return render_template('results.html', weather_data=weather_data, accommodations_data=accommodations_data, sights_data=sights_data, food_and_drink_data=food_and_drink_data)
 
-# Run the Flask app
+@app.route('/update_weather_language')
+def update_weather_language():
+    language = request.args.get('language')
+    # Fetch weather conditions data from the database using the language
+    weather_data = fetch_weather_conditions_from_db(language)
+    return jsonify(weather_data)
+
 if __name__ == '__main__':
     app.run()
--- V3/P3/InDev/Project3/static/script.js
+++ V3/P3/InDev/Project3/static/script.js
@@ -3,17 +3,147 @@
 const map = L.map('map').setView([0, 0], 12);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
+// Translations for static texts
+var translations = {
+    en: {
+        weatherForecast: "Weather Forecast for the Next Three Days",
+        date: "Date",
+        avgTemp: "Average Temperature",
+        condition: "Condition",
+        maxWindSpeed: "Max Wind Speed",
+        placesOfInterest: "Places of Interest",
+        closestAccommodations: "Closest 3 Accommodations",
+        closestSights: "Closest 3 Sights",
+        closestRestaurants: "Closest 3 Restaurants"
+    },
+    es: {
+        weatherForecast: "Pronóstico del tiempo para los próximos tres días",
+        date: "Fecha",
+        avgTemp: "Temperatura promedio",
+        condition: "Condición",
+        maxWindSpeed: "Velocidad máxima del viento",
+        placesOfInterest: "Lugares de interés",
+        closestAccommodations: "3 alojamientos más cercanos",
+        closestSights: "3 vistas más cercanas",
+        closestRestaurants: "3 restaurantes más cercanos"
+    },
+    fr: {
+        weatherForecast: "Prévisions météo pour les trois prochains jours",
+        date: "Date",
+        avgTemp: "Température moyenne",
+        condition: "Condition",
+        maxWindSpeed: "Vitesse maximale du vent",
+        placesOfInterest: "Lieux d'intérêt",
+        closestAccommodations: "3 hébergements les plus proches",
+        closestSights: "3 sites les plus proches",
+        closestRestaurants: "3 restaurants les plus proches"
+    },
+    it: {
+        weatherForecast: "Previsioni meteo per i prossimi tre giorni",
+        date: "Data",
+        avgTemp: "Temperatura media",
+        condition: "Condizione",
+        maxWindSpeed: "Velocità massima del vento",
+        placesOfInterest: "Luoghi di interesse",
+        closestAccommodations: "3 alloggi più vicini",
+        closestSights: "3 siti più vicini",
+        closestRestaurants: "3 ristoranti più vicini"
+    },
+    de: {
+        weatherForecast: "Wettervorhersage für die nächsten drei Tage",
+        date: "Datum",
+        avgTemp: "Durchschnittstemperatur",
+        condition: "Zustand",
+        maxWindSpeed: "Maximale Windgeschwindigkeit",
+        placesOfInterest: "Sehenswürdigkeiten",
+        closestAccommodations: "3 nächstgelegene Unterkünfte",
+        closestSights: "3 nächstgelegene Sehenswürdigkeiten",
+        closestRestaurants: "3 nächstgelegene Restaurants"
+    }
+};
+
+// Function to handle language change
+function changeLanguage() {
+    var language = document.getElementById('language').value;
+    const { lat, lng } = map.getCenter();
+    getWeatherData(lat, lng, language);
+}
+
 // Function to fetch weather data including forecast for the next three days in imperial units
-async function getWeatherData(lat, lon) {
+async function getWeatherData(lat, lon, language) {
     const apiKey = 'c04ea89d7fcd4a26b5b05602230108'; // WeatherAPI API key
     const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${lat},${lon}&days=3&units=imperial`;
 
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        updateWeatherUI(data);
+        updateWeatherUI(data, language);
     } catch (error) {
         console.error('Error fetching weather data:', error);
+    }
+}
+
+// Function to update the UI with weather data
+function updateWeatherUI(data, language) {
+    const weatherContainer = document.getElementById('weatherContainer');
+    weatherContainer.innerHTML = `<h2>${translations[language]['weatherForecast']}</h2>`;
+    for (let i = 0; i < 3 && i < data.forecast.forecastday.length; i++) {
+        const forecast = data.forecast.forecastday[i].day;
+        weatherContainer.innerHTML += `
+            <h3>Day ${i + 1}</h3>
+            <p>${translations[language]['date']}: ${data.forecast.forecastday[i].date}</p>
+            <p>${translations[language]['avgTemp']}: ${forecast.avgtemp_c}°C</p>
+            <p>${translations[language]['condition']}: ${forecast.condition.text}</p>
+            <p>${translations[language]['maxWindSpeed']}: ${forecast.maxwind_kph} km/h</p>
+        `;
+    }
+}
+
+// Event listener for the search button
+document.getElementById('searchButton').addEventListener('click', async () => {
+    const location = document.getElementById('searchInput').value;
+    if (location.trim() === '') {
+        console.error('Location name cannot be empty.');
+        return;
+    }
+    const coordinates = await getLocationCoordinates(location);
+    if (coordinates) {
+        const { latitude, longitude } = coordinates;
+        map.setView([latitude, longitude], 12);
+        var language = document.getElementById('language').value;
+        await getWeatherData(latitude, longitude, language);
+        const accommodationsData = await getGeoapifyData(latitude, longitude, 'accommodation.hotel');
+        const sightsData = await getGeoapifyData(latitude, longitude, 'entertainment.culture');
+        const foodAndDrinkData = await getGeoapifyData(latitude, longitude, 'catering.restaurant');
+        updateGeoapifyUI(accommodationsData, sightsData, foodAndDrinkData);
+    }
+});
+
+// Function to update the UI with Geoapify data for Accommodations, Sights, and Food & Drink
+function updateGeoapifyUI(accommodationsData, sightsData, foodAndDrinkData) {
+    const geoapifyContainer = document.getElementById('geoapifyContainer');
+    geoapifyContainer.innerHTML = '<h2>Places of Interest</h2>';
+    addMarkersAndData(accommodationsData, 'Accommodations');
+    addMarkersAndData(sightsData, 'Sights');
+    addMarkersAndData(foodAndDrinkData, 'Restaurants');
+}
+
+// Function to add markers to the map and update Geoapify container based on Geoapify data
+function addMarkersAndData(geoapifyData, category) {
+    if (geoapifyData && geoapifyData.features.length > 0) {
+        const geoapifyContainer = document.getElementById('geoapifyContainer');
+        geoapifyContainer.innerHTML += `<h3>Closest 3 ${category}</h3>`;
+        geoapifyData.features.forEach(place => {
+            var marker = L.marker([place.geometry.coordinates[1], place.geometry.coordinates[0]]).addTo(map);
+            var popupContent = `
+                <div class="popup">
+                    <h3>${place.properties.name}</h3>
+                    <p>${place.properties.formatted}</p>
+                </div>
+            `;
+            marker.bindPopup(popupContent);
+            geoapifyContainer.innerHTML += popupContent;
+        });
     }
 }
 
@@ -51,65 +181,16 @@ async function getLocationCoordinates(location) {
     }
 }
 
-// Event listener for the search button
-document.getElementById('searchButton').addEventListener('click', async () => {
-    const location = document.getElementById('searchInput').value;
-    if (location.trim() === '') {
-        console.error('Location name cannot be empty.');
-        return;
-    }
-    const coordinates = await getLocationCoordinates(location);
-    if (coordinates) {
-        const { latitude, longitude } = coordinates;
-        map.setView([latitude, longitude], 12);
-        await getWeatherData(latitude, longitude);
-        const accommodationsData = await getGeoapifyData(latitude, longitude, 'accommodation.hotel');
-        const sightsData = await getGeoapifyData(latitude, longitude, 'entertainment.culture');
-        const foodAndDrinkData = await getGeoapifyData(latitude, longitude, 'catering.restaurant');
-        updateGeoapifyUI(accommodationsData, sightsData, foodAndDrinkData);
-    }
-});
-
-// Function to update the UI with weather data
-function updateWeatherUI(data) {
-    const weatherContainer = document.getElementById('weatherContainer');
-    weatherContainer.innerHTML = '<h2>Weather Forecast for the Next Three Days</h2>';
-    for (let i = 0; i < 3 && i < data.forecast.forecastday.length; i++) {
-        const forecast = data.forecast.forecastday[i].day;
-        weatherContainer.innerHTML += `
-            <h3>Day ${i + 1}</h3>
-            <p>Date: ${data.forecast.forecastday[i].date}</p>
-            <p>Average Temperature: ${forecast.avgtemp_c}°C</p>
-            <p>Condition: ${forecast.condition.text}</p>
-            <p>Max Wind Speed: ${forecast.maxwind_kph} km/h</p>
-        `;
+async function updateWeatherLanguage() {
+    const language = document.getElementById('language').value;
+    try {
+        const response = await fetch(`/update_weather_language?language=${language}`);
+        const data = await response.json();
+        updateWeatherUI(data, language);
+    } catch (error) {
+        console.error('Error updating weather language:', error);
     }
 }
 
-// Function to update the UI with Geoapify data for Accommodations, Sights, and Food & Drink
-function updateGeoapifyUI(accommodationsData, sightsData, foodAndDrinkData) {
-    const geoapifyContainer = document.getElementById('geoapifyContainer');
-    geoapifyContainer.innerHTML = '<h2>Places of Interest</h2>';
-    addMarkersAndData(accommodationsData, 'Accommodations');
-    addMarkersAndData(sightsData, 'Sights');
-    addMarkersAndData(foodAndDrinkData, 'Restaurants');
-}
-
-// Function to add markers to the map and update Geoapify container based on Geoapify data
-function addMarkersAndData(geoapifyData, category) {
-    if (geoapifyData && geoapifyData.features.length > 0) {
-        const geoapifyContainer = document.getElementById('geoapifyContainer');
-        geoapifyContainer.innerHTML += `<h3>Closest 3 ${category}</h3>`;
-        geoapifyData.features.forEach(place => {
-            var marker = L.marker([place.geometry.coordinates[1], place.geometry.coordinates[0]]).addTo(map);
-            var popupContent = `
-                <div class="popup">
-                    <h3>${place.properties.name}</h3>
-                    <p>${place.properties.formatted}</p>
-                </div>
-            `;
-            marker.bindPopup(popupContent);
-            geoapifyContainer.innerHTML += popupContent; // Added line to update Geoapify container
-        });
-    }
-}
+// Event listener for language selector
+document.getElementById('language').addEventListener('change', updateWeatherLanguage);
